b12996e3d69b0d85ec89c17f99af2471904520a8
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-10-07 10:42:00 +0200

    added feature section

--- src/components/featuresSection/imgFeatures/imgFeatures.tsx
+++ src/components/featuresSection/imgFeatures/imgFeatures.tsx
@@ -1,9 +1,14 @@
+import styles from './../../../_App.module.scss'
 import type { FeaturesImgProps } from './featuresTypes'
 
 function ImgFeatures({ src, alt, style }: FeaturesImgProps) {
     return (
         <>
+        <div className={`${styles["col-12"]} ${styles["col-md-6"]}`}>
+            <div className={`${styles["position-relative"]}`}>
             <img src={src} className={style} alt={alt} />
+            </div>
+        </div>
         </>
     )
 }
