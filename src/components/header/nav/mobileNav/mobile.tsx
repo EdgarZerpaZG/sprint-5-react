aee3e7c1af542dca780b2f5b60bdd75db2dfaaf5
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-10-13 12:14:53 +0200

    added use state in mobile menu

--- src/components/header/nav/mobileNav/mobile.tsx
+++ src/components/header/nav/mobileNav/mobile.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styles from './../../../../_App.module.scss'
 import mobileStyles from './mobile.module.scss'
 import Menu from '/icon-hamburger.svg'
@@ -7,23 +8,38 @@ import Facebook from '/icon-facebook.svg'
 import Twitter from '/icon-twitter.svg'
 
 export default function MobileNav() {
+
+const [open, setOpen] = useState(false)
+
   return (
     <>
         <div id="menu-mobile-btn" className={`${styles["d-none"]} ${styles["d-md-block"]} ${styles["h-100"]}`}>
             <div className={`${mobileStyles["menu-mobile-box"]} ${styles["h-100"]} ${styles["d-flex"]} ${styles["justify-content-center"]} ${styles["align-items-center"]}`}>
-                <img className={`${styles["img-fluid"]}`} src={Menu} alt="Bookmark"/>
+                <img
+                    className={`${styles["img-fluid"]}`}
+                    src={Menu}
+                    alt="Open Menu BookMarks"
+                    onClick={() => setOpen(true)}
+                />
             </div>
         </div>
-        <div id="menu-mobile" className={`${mobileStyles["menu-mobile"]} ${styles["d-none"]} ${styles["d-md-block"]}`}>
-            <div id="menu-mobile-close" className="btn-close-box">
-                <img className={`${mobileStyles["btn-close"]} ${styles["img-fluid"]}`} src={Close} alt="Bookmark"/>
+        <div id="menu-mobile" className={`${mobileStyles["menu-mobile"]} ${
+          open ? mobileStyles["menu-open"] : mobileStyles["menu-closed"]
+        } ${styles["d-none"]} ${styles["d-md-block"]}`}>
+            <div id="menu-mobile-close" className={`${mobileStyles["btn-close-box"]}`}>
+                <img
+                    className={`${mobileStyles["btn-close"]} ${styles["img-fluid"]}`}
+                    src={Close}
+                    alt="Close Menu"
+                    onClick={() => setOpen(false)}
+                />
             </div>
             <div className={`${mobileStyles["menu-mobile-logo"]}`}>
                 <img className={`${styles["img-fluid"]}`} src={BookmarkWhite} alt="Bookmark"/>
             </div>
             <div className={`${mobileStyles["menu-mobile-content"]}`}>
                 <div className={`${styles["h-100"]} ${styles["w-100"]}`}>
-                    <div className={`${styles["col-12"]} ${styles["h-100"]}"`}>
+                    <div className={`${styles["col-12"]} ${styles["h-100"]}`}>
                         <div className={`${mobileStyles["menu-mobile-list"]}`}>
                             <ul className={`${mobileStyles["header-menu-mobile"]} ${mobileStyles["text-uppercase"]}`}>
                                 <li className={`${mobileStyles["menu-item"]}`}>Features</li>
